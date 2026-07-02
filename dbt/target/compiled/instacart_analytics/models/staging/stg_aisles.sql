33128f1dae4ac5824b6b5fc96656c94247f59a05
Naga Sai Kartikeya Vemula <vnskartikeya@gmail.com>
2026-07-01 23:48:50 -0400

    Ignore generated dbt artifacts

--- dbt/target/compiled/instacart_analytics/models/staging/stg_aisles.sql
+++
@@ -1,4 +0,0 @@
-select
-    aisle_id::integer as aisle_id,
-    aisle::varchar as aisle_name
-from "instacart_db"."raw"."aisles"
--- dbt/target/compiled/instacart_analytics/models/staging/stg_departments.sql
+++
@@ -1,4 +0,0 @@
-select
-    department_id::integer as department_id,
-    department::varchar as department_name
-from "instacart_db"."raw"."departments"
